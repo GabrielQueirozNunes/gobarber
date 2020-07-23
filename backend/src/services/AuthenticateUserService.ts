8a3f9c97e26dec0d2693084032dc1ef2577b58d5
Gabriel Queiroz Nunes <gabrielquenu@gmail.com>
2020-07-23 08:45:08 -0300

    feature: middleware to ensure authentication created

--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -32,9 +33,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination');
     }
 
-    const token = sign({}, '9fe5159722f842a4ae12515d29204f90', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return { user, token };
