cf15c59ca86085922644de9e540316f3ff201467
Gabriel Queiroz Nunes <gabrielquenu@gmail.com>
2021-03-11 19:34:54 -0300

    feature: e-mail template developed

--- server/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ server/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,8 +1,7 @@
-import IParseMailTemplateDTO from '@shared/container/providers/MailTemplateProvider/dtos/IParseMailTemplateDTO';
 import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
 
 export default class FakeMailTemplateProvider implements IMailTemplateProvider {
-  public async parse({ template }: IParseMailTemplateDTO): Promise<string> {
-    return template;
+  public async parse(): Promise<string> {
+    return 'Mail content';
   }
 }
